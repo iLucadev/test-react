ebd4e973c6c713e74933a58439bf7773a4709150
Lucas Iriarte <lucasdiriarte@gmail.com>
2021-01-29 14:28:19 -0300

    Desafio NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
 import { MenuItems } from "./MenuItems";
 
-class NavBar extends Component {
+function NavBar(params) {
+  return (
+    <nav className="NavBarItems">
+      <h1 className="navbar-logo">Bootlegger </h1>
+      <div className="menu-icon"></div>
+
+      <ul>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
+
+/* class NavBar extends Component {
   render() {
     return (
       <nav className="NavBarItems">
@@ -22,6 +45,4 @@ class NavBar extends Component {
       </nav>
     );
   }
-}
-
-export default NavBar;
+} */
